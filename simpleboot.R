d11daf221df82e07baf0089e5aa6d8ddabda4443
jarvisc1 <christopher1.jarvis@gmail.com>
2015-10-22 12:38:31 +0100

    Added in Quantile functions with prob argument.

--- simpleboot.R
+++ simpleboot.R
@@ -1,12 +1,17 @@
-simpleboot<-function(x,y=NULL,stat,reps=1000) {
+simpleboot<-function(x,y=NULL,stat, prob = NULL, reps=1000) {
   cat("Bootstrapping can go wrong!\n")
   cat("This simple function will not show you warning messages.\n")
   cat("Check results closely and be prepared to consult a statistician.\n")
   if(stat=="max" | stat=="min") { warning("Bootstrap is likely to be incorrect for minima and maxima") }
-  if(stat!="mean" & stat!="median" & stat!="p25" & stat!="p75" & stat!="iqr" & 
+  if(stat!="mean" & stat!="median" & stat!="p25" & stat!="p75" & stat!="iqr" & stat!="quantile" &
      stat!="sd" & stat!="pearson" & stat!="spearman" & stat!="meandiff" & stat!="mediandiff") {
-        warning("Simpleboot is only programmed to work with stat set to one of: mean, median, sd, iqr, p25, p75, pearson, spearman, meandiff, mediandiff. Other functions might work, but there's no guarantee!")
+        warning("Simpleboot is only programmed to work with stat set to one of: mean, median, sd, iqr, p25, p75, quantile, pearson, spearman, meandiff, mediandiff. Other functions might work, but there's no guarantee!")
   }
+  if(stat == "quantile" & (prob <= 0 | prob > 1)){
+    stop("Quantile values must be greater than zero and less than one.")
+    
+  }
+
   require(boot)
   if(stat=="p25") {
     eval(parse(text=eval(substitute(paste("p.func<-function(x,i) quantile(x[i],probs=0.25,na.rm=TRUE)",sep=""),list(stat=stat)))))
@@ -16,6 +21,9 @@ simpleboot<-function(x,y=NULL,stat,reps=1000) {
   }
   else if(stat=="iqr") {
     eval(parse(text=eval(substitute(paste("p.func<-function(x,i) quantile(x[i],probs=0.75,na.rm=TRUE)-quantile(x[i],probs=0.25,na.rm=TRUE)",sep=""),list(stat=stat)))))
+  }
+  else if(stat=="quantile" & prob ) {
+    eval(parse(text=eval(substitute(paste("p.func<-function(x,i) quantile(x[i],probs=",prob,",na.rm=TRUE)",sep=""),list(stat=stat)))))
   }
   else if(stat=="pearson" | stat=="spearman") {
     x<-matrix(c(x,y),ncol=2)[(!is.na(x))&(!is.na(y)),]
